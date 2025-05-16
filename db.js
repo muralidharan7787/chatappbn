7d1dd48b378a9d7fd6b4ac0acc9f4ade42d1ff28
muralidharan7787 <muralidharan25aug@gmail.com>
2025-05-16 07:16:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,30 +1,4 @@
 const sql = require('mssql');
-require('dotenv').config();
-
-
-// const config = {
-//     user: "muralidharan@development-md",
-//     password: "9363509@Murali",  // 🔐 Replace with actual password
-//     server: "development-md.database.windows.net",
-//     database: "development",
-//     port: 1433,
-//     options: {
-//         encrypt: true, // ✅ for Azure
-//         trustServerCertificate: false,
-//     }
-// };
-
-// const config = {
-//     user: "muralidharan@development-md",
-//     password: process.env.DB_PASSWORD,  // 🔐 Replace with actual password
-//     server: "development-md.database.windows.net",
-//     database: "chatapp-db",
-//     port: 1433,
-//     options: {
-//         encrypt: true, // ✅ for Azure
-//         trustServerCertificate: false,
-//     }
-// };
 
 const config = {
     user: process.env.DB_USER,
